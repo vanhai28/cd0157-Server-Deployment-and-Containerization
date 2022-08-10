433e17c239626b1f6dfed2a40f45bb825d97d013
nguyễn văn hải <nvhaik18@gmail.com>
2022-08-10 23:12:22 +0700

    update test file

--- test_main.py
+++ test_main.py
@@ -15,7 +15,7 @@ PASSWORD = 'huff-puff'
 @pytest.fixture
 def client():
     os.environ['JWT_SECRET'] = SECRET
-    main.APP.config['TESTING'] = False
+    main.APP.config['TESTING'] = True
     client = main.APP.test_client()
 
     yield client
